4db03dd859424560f7c01f779bcc2352f7320475
Carlos Rodriguez <rodrigcp22@gmail.com>
2023-05-01 18:36:13 +1000

    Perfection was achieved! Even more! And we have nice graphics now!

--- modules/path_generator/plugins.py
+++ modules/path_generator/plugins.py
@@ -23,14 +23,14 @@ class Path_Generator:
         self.maze_width = maze_size[1]
         self.maze_height = maze_size[0]
 
-        self.x_jump_max_size = jump_max_size[0]
-        self.y_jump_max_size = jump_max_size[1]
+        self.x_jump_max_size = jump_max_size[1]
+        self.y_jump_max_size = jump_max_size[0]
 
-        self.x_start = start_coordinates[0]
-        self.y_start = start_coordinates[1]
+        self.x_start = start_coordinates[1]
+        self.y_start = start_coordinates[0]
 
-        self.x_finish = finish_coordinates[0]
-        self.y_finish = finish_coordinates[1]
+        self.x_finish = finish_coordinates[1]
+        self.y_finish = finish_coordinates[0]
 
         self.x_curr_coordinate = self.x_start
         self.y_curr_coordinate = self.y_start
@@ -49,7 +49,7 @@ class Path_Generator:
 
         # Clean move stack, so it doesn't preserve moves from previous calls
         self.coordinates_stack = []
-        self.maze = [['#'] * self.maze_width for row in range(self.maze_height)]
+        self.maze = [['🔴'] * self.maze_width for row in range(self.maze_height)]
 
         x_step = 0
         y_step = 0
@@ -58,32 +58,48 @@ class Path_Generator:
         while self.x_distance and self.y_distance != 0:
             # 1- Determine (update) distance between current coordinate / finish in x/y
             self._update_xy_distance()
+            # self._update_local_distance()
 
             # 2- Generate a random int between 0 and the minimum between the current x/y distance and the max jump size
-            x_step = random.randint(0, min(self.x_distance, self.x_jump_max_size))
-            y_step = random.randint(0, min(self.y_distance, self.y_jump_max_size))
-
-            # 3- Calculate x/y coordinate for next move
-
-            self.x_future_coordinate = self.x_curr_coordinate + x_step
-            self.y_future_coordinate = self.y_curr_coordinate + y_step
-
-            # 4- Append x/y move to move stack
+            x_step = random.randint(0, min(abs(self.x_distance), abs(self.x_jump_max_size)))
+            y_step = random.randint(0, min(abs(self.y_distance), abs(self.y_jump_max_size)))
 
             self._update_local_distance()
 
-            for i in range(0, self.y_local_distance + 1):
-                self.coordinates_temp_stack.append([self.y_curr_coordinate + i, self.x_curr_coordinate])
 
+
+
+            if self.x_distance < 0: x_step = -x_step
+
+            if self.y_distance < 0: y_step = -y_step
+
+            # 3- Calculate x/y coordinate for next move
+            self.x_future_coordinate = self.x_curr_coordinate + x_step
+            self.y_future_coordinate = self.y_curr_coordinate + y_step
+            self._update_local_distance()
+
+
+            # 4- Append x/y move to move stack
+            if self.x_local_distance < 0:
+                for i in range(0, abs(self.x_local_distance - 1)):
+                    self.coordinates_temp_stack.append([self.y_curr_coordinate, self.x_curr_coordinate - i])
+            else:
+                for i in range(0, self.x_local_distance + 1):
+                    self.coordinates_temp_stack.append([self.y_curr_coordinate, self.x_curr_coordinate + i])
+
+            self.x_curr_coordinate = self.x_future_coordinate
+
+            if self.y_local_distance < 0:
+                for i in range(0, abs(self.y_local_distance - 1)):
+                    self.coordinates_temp_stack.append([self.y_curr_coordinate - i, self.x_curr_coordinate])
+            else:
+                for i in range(0, self.y_local_distance + 1):
+                    self.coordinates_temp_stack.append([self.y_curr_coordinate + i, self.x_curr_coordinate])
             self.y_curr_coordinate = self.y_future_coordinate
-
-            for i in range(0, self.x_local_distance + 1):
-                self.coordinates_temp_stack.append([self.y_curr_coordinate, self.x_curr_coordinate + i])
-
             # 5- Repeat until distance is 0 for x/y
 
             self.coordinates_stack.append([self.y_curr_coordinate, self.x_curr_coordinate])
-            self.x_curr_coordinate += x_step
+            # self.x_curr_coordinate += x_step
             # self.y_curr_coordinate += y_step
 
         print(self.coordinates_stack)
@@ -112,13 +128,12 @@ class Path_Generator:
         :return:
         """
         for item in self.coordinates_stack:
-            self.maze[item[0]][item[1]] = "."
+            self.maze[item[0]][item[1]] = "🟢"
 
         for row in self.maze:
             print(row)
 
+
 # Remove, just for testing
-path_maze = Path_Generator([40, 40], [5, 5], [39, 38], [15, 36])
+path_maze = Path_Generator((40, 40), (5, 5), (38, 39), (1, 1))
 path_maze.generate_direct_path()
-
-
